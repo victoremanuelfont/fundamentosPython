4d1c13b8691c1dcc9049081b297f01da7d417958
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-16 15:17:42 -0300

    Conversão de variáveis

--- variaveis.py
+++ variaveis.py
@@ -30,5 +30,39 @@ numnero_complexo = 3 + 4j
 sensor_temperatura = 28 # Variável com todas as letras minúsculas
 Sensor_temperatura = 32 # Variável com a primeira letra maiúscula
 
+#Conversão de variáveis
 
+#String parta Inteiro
+numero_str = "100"
+numero_int = int(numero_str)
+print (numero_int) #sai´da: 100
 
+#String para float
+numero_str = "100.5"
+numero_float = float(numero_str)
+print(numero_float) #saida: 100.5
+
+#Inteiro para string
+numero_int = 100
+numero_str = str(numero_int)
+print(numero_str)
+
+#Conversão de string para booleano
+boolean_str = "True"
+boolean_val = bool(boolean_str) #Converte a string "True" patra um booleano True
+print(boolean_val) #Saída: True
+
+#Conversão de inteiro para booleano
+boolean_int = 1 
+boolean_val = bool(boolean_int)
+print(boolean_val) # a saída vai ser true porque o int é 1, se fosse 0 a saída é false
+
+#Conversão de float para booleano
+numero_float = 0.0
+boolean_val = bool(numero_float) #Qualquer numero diferente de ZERO é true. 0.0=0=false
+print(boolean_val)
+
+# Conversão de booleano para string
+boolean_val = True
+boolean_str = str(boolean_val) # Converte o booleano True para uma string "True"
+print(boolean_str) # Saída: "True"
