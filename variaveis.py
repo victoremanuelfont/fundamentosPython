e3070bdf8ea748011c52a3d2456156e51aca0617
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-16 14:44:56 -0300

    Variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,34 @@
+#Declaração de variáveis
+
+#inteiro
+contagem_eventos = 150
+
+#float
+sensor_temperatura = 25.5
+
+#string
+nome_dispositivo = "SensorX100"
+
+#Booleano
+status_operacional = True
+
+#Número complexo 
+numnero_complexo = 3 + 4j
+
+# Erros comuns
+# Nome de variável não pode começar com número
+# 1numero = 10 
+
+# Nome de variável não pode conter caracteres especiais
+# @sensor = 20
+
+# 'for' é uma palavra reservada
+# for = 30
+
+# As variáveis têm o mesmo nome, porém, o Python interpreta que são
+# variáveis diferentes devido ao uso de maiúsculas e minúsculas
+sensor_temperatura = 28 # Variável com todas as letras minúsculas
+Sensor_temperatura = 32 # Variável com a primeira letra maiúscula
+
+
+
