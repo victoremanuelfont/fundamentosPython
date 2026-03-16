ceda34e78d2360eb87d81ee09f6ff6d5aaa92923
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-16 15:40:40 -0300

    Operaçoes com variáveis

---
+++ operacoes.py
@@ -0,0 +1,48 @@
+a = 2
+b = 4
+c = 6
+
+#Operacoes matemáticas
+print(a + b)
+print(b - a)
+print(c * b)
+print(c / b)
+print("__________________")
+
+#operadores de comparacao
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= c)
+print(a <= c)
+print("__________________")
+
+#operadores lógicos and, or, not
+print(a == b and b < c)
+print(a != b and b < c) #and se um for false a saída vai ser false
+print(b > a or b >= c) #or basta um ser verdadeiro para a saída ser true
+print(b < a or c > b)
+print(not (a == c)) # not inverte o valor
+print(not (b <= a or b > c))
+print("__________________")
+
+
+print(c % b) #resto da divisão
+print(a ** b) #Exponeciação
+print(c // b) #divisão inteira, descarta a parte decimal 
+print("__________________")
+
+
+#Expressoes aritméticas
+print( a + b * c)  #Multiplicação primeiro
+print ((a + b)* c) #primeiro dentro do parentese 
+print("__________________")
+
+
+
+
+
+
+
+
