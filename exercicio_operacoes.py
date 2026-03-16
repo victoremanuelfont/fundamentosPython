594e9eece6803aea980931c83532ab60dd8a6f9c
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-16 15:57:07 -0300

    Exercicio

---
+++ exercicio_operacoes.py
@@ -0,0 +1,20 @@
+a = 7
+b = 3
+c = 5
+d = 2
+
+expressao = ((a * b) + (c / d)) - (a ** b)
+print(f"resultado = {expressao}")
+#resultado = -319.5
+print("___________________")
+
+x = 10
+y = 20
+z = 30
+
+expressao_condicional = (x < y and y > z) or (x + y == z and not (z == x * 3))
+print(f"resultado = {expressao_condicional}")
+#resultado = False
+print("___________________")
+
+
