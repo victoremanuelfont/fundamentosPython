5c99c32f74b458f61f21b340d02fc4962bafcc29
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-20 14:24:18 -0300

    Operaçoes com dicionários

---
+++ dicionarios.py
@@ -0,0 +1,60 @@
+#Lista mas que armazena varios dados tipados
+
+sensor = {"id": 1, "tipo": "temperatura"}
+print(sensor["id"])
+print(sensor["tipo"])
+print("___________________")
+
+#Alterando o dicionario
+sensor["id"] = 2
+print(sensor["id"])
+
+#Imprimir todo o dicionario
+print(sensor)
+print("___________________")
+
+#Operaçoes com dicionarios
+estados_maquinas = {
+    "maquina_1": "operacional",
+    "maquina_2": "manutencao",
+    "maquina_3": "parada"
+}
+
+print(estados_maquinas)
+print("___________________")
+
+#Adicionando novos estados ao dicionarios
+estados_maquinas.update({"maquina_4": "operacional"})
+print(estados_maquinas)
+print("___________________")
+
+#Acessando estados especi´ficos
+print(estados_maquinas["maquina_3"])
+print("___________________")
+
+#Alterando 
+estados_maquinas["maquina_3"] = "Operacional"
+print(estados_maquinas)
+print("___________________")
+
+#Removendo um estado
+del estados_maquinas["maquina_3"] 
+print(estados_maquinas)
+print("___________________")
+
+#Listando todas as máquinas cadastradas/ lista os indices
+print(estados_maquinas.keys())
+print("___________________")
+
+#Definindo um dicionário dentro do dicionário
+comandos_maquinas = {
+    "maquina_A": ["ligar", "monitorar", "desligar"], 
+    "maquina_B": ["manutencao", "calibrar"], 
+    "maquina_C": ["CHORO", "DESESPERO"]
+}
+
+print(comandos_maquinas)
+print("___________________")
+
+#Acessando o indice
+print(comandos_maquinas["maquina_A"].pop(0))
