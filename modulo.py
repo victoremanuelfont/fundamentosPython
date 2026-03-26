b1e58990cb54294e19385b5758a38f53e03d08ed
Victor Emanuel <victoremanuelfont3@gmail.com>
2026-03-26 10:24:00 -0300

    Uso de modulos

---
+++ modulo.py
@@ -0,0 +1,23 @@
+#Modulos
+#import nome_do_modulo
+# funcao sqrt(raiz quadrada)
+
+import math
+
+variancia = 25
+desvio_padrao = math.sqrt(variancia)
+print(desvio_padrao)
+
+print("--------------------------------------")
+
+
+#from nome_do_modulo import item_especifico
+from datetime import date
+data_hoje = date.today()
+print(data_hoje)
+print("--------------------------------------")
+
+#Utilizando outra classe como modulo e utilizando a funcao
+from funcoes import exibir_nome
+print("--------------------------------------")
+
